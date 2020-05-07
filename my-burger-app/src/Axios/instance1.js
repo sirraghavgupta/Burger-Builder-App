883f09264055fe5fcdd55ea829f94a17768b44ba
raghavGuptaTTN <raghav.gupta@tothenew.com>
2020-05-07 20:35:28 +0530

    created an axios instance

---
+++ my-burger-app/src/Axios/instance1.js
@@ -0,0 +1,15 @@
+import Axios from 'axios';
+
+/**
+ * we are creating our own instance here, because we will need a different 
+ * base url different purposes like authentication. ,
+ * so, we will need separate one for all. 
+ */
+
+const instance = Axios.create(
+    {
+        baseURL : 'https://my-burger-app-e5ba5.firebaseio.com/'
+    }
+);
+
+export default instance;
