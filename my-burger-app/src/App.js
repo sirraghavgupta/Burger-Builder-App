6b793a4718e3e94884e1af8b0224aecc779731b0
raghavGuptaTTN <raghav.gupta@tothenew.com>
2020-05-11 10:42:54 +0530

    added orders component and route

--- my-burger-app/src/App.js
+++ my-burger-app/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Layout from './containers/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
+import Orders from './containers/Orders/Orders';
 import { Route, Switch} from 'react-router-dom';
 
 class App extends Component {
@@ -21,6 +22,7 @@ class App extends Component {
 
         <Switch>
           <Route path = "/checkout" component = {Checkout} />
+          <Route path = "/orders" component = {Orders} />
           <Route path = "/" component = {BurgerBuilder} />
         </Switch>
     
