9fc35c6da242cd480a02256019e469a624dd767d
raghavGuptaTTN <raghav.gupta@tothenew.com>
2020-05-16 16:30:09 +0530

    different order button for authenticated and unauth.. user, redirecting to login page if user is unauth...

--- my-burger-app/src/components/Burger/BuildControls/BuildControls.js
+++ my-burger-app/src/components/Burger/BuildControls/BuildControls.js
@@ -2,27 +2,25 @@ import React from 'react';
 import classes from './BuildControls.module.css';
 import BuildControl from './BuildControl/BuildControl';
 
-
-
 const BuildControls = (props) => (
-
-    <div className = { classes.BuildControls }>
-        <p>Current Price : <strong>{ props.price.toFixed(2) }</strong></p>
-        { 
-        props.ingredientNames.map(  
-            (ingredientName) => <BuildControl label = {ingredientName} 
-            // key has been kept like this because its unique here. 
-                                       key = { ingredientName } 
-                                       added = { () => props.ingredientAdded( ingredientName ) }
-                                       removed = { () => props.ingredientRemoved( ingredientName ) }
-                                       disabled = { props.disabled[ ingredientName ] } />
-        ) }
-        <button className = { classes.OrderButton }
-                disabled = { !props.purchasable }
-                onClick = { props.ordered }
-        >Order Now</button>
-    </div>
-
+  <div className={classes.BuildControls}>
+    <p>
+      Current Price : <strong>{props.price.toFixed(2)}</strong>
+    </p>
+    {props.ingredientNames.map((ingredientName) => (
+      <BuildControl
+        label={ingredientName}
+        // key has been kept like this because its unique here.
+        key={ingredientName}
+        added={() => props.ingredientAdded(ingredientName)}
+        removed={() => props.ingredientRemoved(ingredientName)}
+        disabled={props.disabled[ingredientName]}
+      />
+    ))}
+    <button className={classes.OrderButton} disabled={!props.purchasable} onClick={props.ordered}>
+      {props.isAuth ? 'Order Now' : 'Signup To Continue'}
+    </button>
+  </div>
 );
 
 export default BuildControls;
--- my-burger-app/src/containers/BurgerBuilder/BurgerBuilder.js
+++ my-burger-app/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,136 +5,125 @@ import * as burgerBuilderActions from '../../store/actions/index';
 import Aux from '../../hoc/Aux/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
-import Modal from '../../components/UI/Modal/Modal'
+import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import axios from '../../Axios/instance1';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
+class BurgerBuilder extends Component {
+  state = {
+    purchasing: false,
+  };
 
+  componentDidMount = () => {
+    console.log('getting ingredients from server - componentDidMount()');
+    this.props.onInitIngredients();
+  };
 
-class BurgerBuilder extends Component{
+  getPurchaseState = (ingredients) => {
+    const count = Object.values(ingredients).reduce((a, b) => a + b, 0);
+    let purchaseState = count > 0 ? true : false;
+    return purchaseState;
+  };
 
-    state = {
-        purchasing : false
+  /**
+   * basically, we want to arrow function here because we need to refer to
+   * this. now, we refer to this in render() also but we use normal syntax
+   * there. the issue comes when the method is triggered by an event. then
+   * there is a problem in using the this. so, its preferred to use arrow
+   * function always so that we dont miss anything.
+   */
+  purchaseHandler = () => {
+    if (this.props.isAuthenticated) {
+      this.setState({ purchasing: true });
+    } else {
+      this.props.history.push('/auth');
+    }
+  };
+
+  purchaseCancelHandler = () => {
+    this.setState({ purchasing: false });
+  };
+
+  purchaseContinueHandler = () => {
+    console.log('ordering the item');
+    this.props.onInitPurchase();
+    this.props.history.push('/checkout');
+  };
+
+  render() {
+    const disabledInfo = { ...this.props.ingredients };
+    Object.keys(disabledInfo).forEach((type) => {
+      disabledInfo[type] = disabledInfo[type] <= 0;
+    });
+
+    let burger = this.props.error ? <p>Something went wrong!!</p> : <Spinner />;
+    let orderSummary = null;
+    if (this.props.ingredients) {
+      burger = (
+        <Aux>
+          <Burger ingredients={this.props.ingredients} />
+
+          <BuildControls
+            ingredientNames={Object.keys(this.props.ingredients)}
+            ingredientAdded={this.props.onIngredientAdded}
+            ingredientRemoved={this.props.onIngredientRemoved}
+            disabled={disabledInfo}
+            price={this.props.totalPrice}
+            purchasable={this.getPurchaseState(this.props.ingredients)}
+            ordered={this.purchaseHandler}
+            isAuth={this.props.isAuthenticated}
+          />
+        </Aux>
+      );
+
+      orderSummary = (
+        <OrderSummary
+          ingredients={this.props.ingredients}
+          cancelled={this.purchaseCancelHandler}
+          continued={this.purchaseContinueHandler}
+          price={this.props.totalPrice}
+        />
+      );
     }
 
-    componentDidMount = () => {
-        console.log("getting ingredients from server - componentDidMount()");
-        this.props.onInitIngredients();
-    }
+    return (
+      <Aux>
+        <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
+          {orderSummary}
+        </Modal>
 
-    getPurchaseState = (ingredients) => {
-        const count = Object.values(ingredients).reduce( (a,b) => a+b , 0  );
-        let purchaseState = count>0 ? true: false;
-        return purchaseState;
-    }   
-
-    /**
-     * basically, we want to arrow function here because we need to refer to 
-     * this. now, we refer to this in render() also but we use normal syntax 
-     * there. the issue comes when the method is triggered by an event. then 
-     * there is a problem in using the this. so, its preferred to use arrow
-     * function always so that we dont miss anything. 
-     */
-    purchaseHandler = () => {
-        this.setState( { purchasing : true } );
-    }
-    
-    purchaseCancelHandler = () => {
-        this.setState( { purchasing : false } );
-    }
-
-    purchaseContinueHandler = () => {
-        console.log("ordering the item");
-        this.props.onInitPurchase();    
-        this.props.history.push('/checkout');
-    }
-
-    render(){
-
-        const disabledInfo = {...this.props.ingredients};
-        Object.keys(disabledInfo).forEach((type)=>{
-            disabledInfo[type] = disabledInfo[type] <=0;
-        });
-        
-        let burger = this.props.error ? <p>Something went wrong!!</p> : <Spinner/>;
-        let orderSummary = null;
-        if(this.props.ingredients){
-            burger = (
-                <Aux>
-                    <Burger ingredients = { this.props.ingredients} />
-        
-                    <BuildControls 
-                        ingredientNames = { Object.keys(this.props.ingredients) }
-                        ingredientAdded = { this.props.onIngredientAdded }
-                        ingredientRemoved = { this.props.onIngredientRemoved }
-                        disabled = { disabledInfo }
-                        price = { this.props.totalPrice }
-                        purchasable = { this.getPurchaseState(this.props.ingredients) }
-                        ordered = { this.purchaseHandler } 
-                    />
-                </Aux>
-            );
-
-            orderSummary = (
-                <OrderSummary ingredients = { this.props.ingredients}
-                                  cancelled = { this.purchaseCancelHandler }
-                                  continued = { this.purchaseContinueHandler }
-                                  price = { this.props.totalPrice }/>
-            );
-        }
-
-
-        return (
-            <Aux>
-                {/* // one method is this. 
-                {this.state.purchasing ? 
-                    <Modal>
-                        <OrderSummary ingredients = {this.props.ingredients}/>
-                    </Modal>
-                    : null
-                } */}
-
-                <Modal show = { this.state.purchasing }
-                       modalClosed = { this.purchaseCancelHandler }>
-                       { orderSummary }
-                </Modal>
-
-                {burger}
-
-            </Aux>
-        );
-    }
+        {burger}
+      </Aux>
+    );
+  }
 }
 
+const mapStateToProps = (state) => {
+  return {
+    ingredients: state.burgerBuilder.ingredients,
+    totalPrice: state.burgerBuilder.totalPrice,
+    error: state.burgerBuilder.error,
+    isAuthenticated: state.auth.token !== null,
+  };
+};
 
-const mapStateToProps = state => {
-
-    return {
-        ingredients : state.burgerBuilder.ingredients,
-        totalPrice : state.burgerBuilder.totalPrice,
-        error : state.burgerBuilder.error
-    };
-}
-
-const mapDispatchToProps = dispatch => {
-
-    return {
-        onIngredientAdded : (name) => dispatch( burgerBuilderActions.addIngredient(name) ),
-        onIngredientRemoved : (name) => dispatch( burgerBuilderActions.removeIngredient(name) ),
-        onInitIngredients : () => dispatch( burgerBuilderActions.initIngredients() ),
-        onInitPurchase : () => dispatch( burgerBuilderActions.purchaseInit() )
-    };
-}
-
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onIngredientAdded: (name) => dispatch(burgerBuilderActions.addIngredient(name)),
+    onIngredientRemoved: (name) => dispatch(burgerBuilderActions.removeIngredient(name)),
+    onInitIngredients: () => dispatch(burgerBuilderActions.initIngredients()),
+    onInitPurchase: () => dispatch(burgerBuilderActions.purchaseInit()),
+  };
+};
 
 /**
- * here we might doubt that we may have problem because there is already 
- * an hoc present to wrap the BurgerBuilder. 
+ * here we might doubt that we may have problem because there is already
+ * an hoc present to wrap the BurgerBuilder.
  * answer is - we may have any number of HOCs, it doesnt matter.
  * plus, the connect will add some props to the withErrorHandler and those
- * props might be lost, but here we are passing the props to the 
- * burger builder inside the withErrorHandler. so, its good that we do that. 
+ * props might be lost, but here we are passing the props to the
+ * burger builder inside the withErrorHandler. so, its good that we do that.
  */
 export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(BurgerBuilder, axios));
