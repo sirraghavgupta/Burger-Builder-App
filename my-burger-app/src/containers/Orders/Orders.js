b02f3231a892a88ce1e8e19d39ae7f7625ed12a0
raghavGuptaTTN <raghav.gupta@tothenew.com>
2020-05-16 20:25:45 +0530

    showing orders which belong to that user only - querying firebase

--- my-burger-app/src/containers/Orders/Orders.js
+++ my-burger-app/src/containers/Orders/Orders.js
@@ -6,49 +6,37 @@ import Spinner from '../../components/UI/Spinner/Spinner';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 import * as orderActions from '../../store/actions/index';
 
+class Orders extends Component {
+  componentDidMount() {
+    this.props.onInitOrders(this.props.token, this.props.userId);
+  }
 
-class Orders extends Component{
+  render() {
+    let orders = <Spinner />;
 
-
-    componentDidMount(){
-        this.props.onInitOrders( this.props.token );
+    if (!this.props.loading) {
+      orders = this.props.orders.map((order) => {
+        return <Order key={order.id} ingredients={order.ingredients} price={+order.price} />;
+      });
     }
 
-    render(){
-
-        let orders = <Spinner/>;
-
-        if( !this.props.loading ){
-            orders = this.props.orders.map( order => {
-                return (
-                    <Order  key = {order.id}
-                            ingredients = { order.ingredients } 
-                            price = { +order.price }   />
-                );
-            });
-    
-        }
-
-        return (
-            <div>
-                {orders}
-            </div>
-        );
-    }
+    return <div>{orders}</div>;
+  }
 }
 
-const mapStateToProps = state => {
-    return {
-        orders : state.order.orders,
-        loading : state.order.loading,
-        token : state.auth.token
-    };
-}
+const mapStateToProps = (state) => {
+  return {
+    orders: state.order.orders,
+    loading: state.order.loading,
+    token: state.auth.token,
+    userId: state.auth.userId,
+  };
+};
 
-const mapDispatchToProps = dispatch => {
-    return {
-        onInitOrders : ( token ) => dispatch( orderActions.fetchOrders( token ) )
-    };
-}
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onInitOrders: (token, userId) => dispatch(orderActions.fetchOrders(token, userId)),
+  };
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(Orders, axios));
--- my-burger-app/src/store/actions/order.js
+++ my-burger-app/src/store/actions/order.js
@@ -1,89 +1,95 @@
 import * as actionTypes from './actionTypes';
 import axios from '../../Axios/instance1';
 
+export const puchaseBurger = (newOrder, token) => {
+  return (dispatch) => {
+    dispatch(purchaseBurgerStart());
 
-export const puchaseBurger = ( newOrder, token ) => {
-    return dispatch => {
-    
-        dispatch( purchaseBurgerStart() );
+    axios
+      .post('/orders.json?auth=' + token, newOrder)
 
-        axios.post("/orders.json?auth="+token, newOrder)
+      .then((response) => {
+        dispatch(purchaseBurgerSuccess(response.data.name, newOrder));
+      })
 
-             .then( response => {
-                        dispatch( purchaseBurgerSuccess(response.data.name, newOrder) );
-                    })
-
-             .catch( error => {
-                        dispatch( purchaseBurgerFailed() );
-                    })
-    }
-}
+      .catch((error) => {
+        dispatch(purchaseBurgerFailed());
+      });
+  };
+};
 
 export const purchaseBurgerStart = () => {
-    return {
-        type : actionTypes.PURCHASE_BURGER_START
-    }
-}
+  return {
+    type: actionTypes.PURCHASE_BURGER_START,
+  };
+};
 
-export const purchaseBurgerSuccess = ( id, newOrder ) => {
-    return {
-        type : actionTypes.PURCHASE_BURGER_SUCCESS,
-        orderId : id,
-        orderData : newOrder
-    }
-}
+export const purchaseBurgerSuccess = (id, newOrder) => {
+  return {
+    type: actionTypes.PURCHASE_BURGER_SUCCESS,
+    orderId: id,
+    orderData: newOrder,
+  };
+};
 
-export const purchaseBurgerFailed = ( ) => {
-    return {
-        type : actionTypes.PURCHASE_BURGER_FAILED
-    }
-}
-
+export const purchaseBurgerFailed = () => {
+  return {
+    type: actionTypes.PURCHASE_BURGER_FAILED,
+  };
+};
 
 export const purchaseInit = () => {
-    return {
-        type : actionTypes.PURCHASE_INIT
-    }
-}
-
+  return {
+    type: actionTypes.PURCHASE_INIT,
+  };
+};
 
 export const fetchOrdersStart = () => {
-    return {
-        type : actionTypes.FETCH_ORDERS_START
-    };
-}
+  return {
+    type: actionTypes.FETCH_ORDERS_START,
+  };
+};
 
-export const fetchOrdersSuccess = ( orders ) => {
-    return {
-        type : actionTypes.FETCH_ORDERS_SUCCESS,
-        orders : orders
-    };
-}
+export const fetchOrdersSuccess = (orders) => {
+  return {
+    type: actionTypes.FETCH_ORDERS_SUCCESS,
+    orders: orders,
+  };
+};
 
 export const fetchOrdersFailed = () => {
-    return {
-        type : actionTypes.FETCH_ORDERS_FAILED
-    };
-}
+  return {
+    type: actionTypes.FETCH_ORDERS_FAILED,
+  };
+};
 
-export const fetchOrders = ( token ) => {
-    return dispatch => {
-        dispatch( fetchOrdersStart() );
-        axios.get("/orders.json?auth="+token)
-             .then( response => {
-                const fetchedOrders  = [];
-                for(let key in response.data){
-                    fetchedOrders.push({
-                        id : key,
-                        ...response.data[key]
-                    });
-                }
-                dispatch( fetchOrdersSuccess(fetchedOrders) );
-             })
-             .catch( error => {
-                 console.log(error);
-                 dispatch( fetchOrdersFailed() );
-             })
-
-    }
-}
+export const fetchOrders = (token, userId) => {
+  return (dispatch) => {
+    dispatch(fetchOrdersStart());
+    /***
+     * in firebase while sending query params like this, we need to use
+     * orderBy always and then equalTo after that refers to the same key
+     * specified in the orderBy query.
+     * also, to make the db searcheable by this prop, we need to modify
+     * the indexOn rule in the firebase. and we need to send the prop name
+     * within quotes and values also within quoted if its a string.
+     */
+    const queryParams = '?auth=' + token + '&orderBy="userId"&equalTo="' + userId + '"';
+    axios
+      .get('/orders.json' + queryParams)
+      .then((response) => {
+        const fetchedOrders = [];
+        for (let key in response.data) {
+          fetchedOrders.push({
+            id: key,
+            ...response.data[key],
+          });
+        }
+        dispatch(fetchOrdersSuccess(fetchedOrders));
+      })
+      .catch((error) => {
+        console.log(error);
+        dispatch(fetchOrdersFailed());
+      });
+  };
+};
