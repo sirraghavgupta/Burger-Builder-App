d118002287bb297e11b5aa043a17d8196ba182ee
raghavGuptaTTN <raghav.gupta@tothenew.com>
2020-05-21 11:18:58 +0530

    converted the logout component

--- my-burger-app/src/containers/Auth/Logout/Logout.js
+++ my-burger-app/src/containers/Auth/Logout/Logout.js
@@ -1,17 +1,15 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import * as authActions from '../../../store/actions/index';
 
-class Logout extends Component {
-  componentDidMount = () => {
-    this.props.onLogout();
-  };
+const Logout = (props) => {
+  useState(() => {
+    props.onLogout();
+  });
 
-  render() {
-    return <Redirect to="/" />;
-  }
-}
+  return <Redirect to="/" />;
+};
 
 const mapDispatchToProps = (dispatch) => {
   return {
